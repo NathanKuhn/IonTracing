688008656bf138084d24ed64ffdefca6109a2b78
NathanKuhn <kuhnnat000@gmail.com>
2026-03-10 18:53:03 -0700

    add some libraries

--- main.py
+++ main.py
@@ -1,128 +1,51 @@
-import pygame
+import astropy.units as u
+import pyvista as pv
 import numpy as np
 
-WIDTH = 300
-HEIGHT = 300
+import magpylib as magpy
+from plasmapy.particles import Particle
+from plasmapy.plasma.grids import CartesianGrid
+from plasmapy.simulation.particle_tracker.particle_tracker import ParticleTracker
+from plasmapy.simulation.particle_tracker.save_routines import IntervalSaveRoutine
+from plasmapy.simulation.particle_tracker.termination_conditions import (
+    TimeElapsedTerminationCondition,
+)
 
-SHOW_FIELD = True
+grid_length = 100
+grid = CartesianGrid(-0.1 * u.m, 0.1 * u.m, num=grid_length)
 
+magnet = magpy.magnet.Cuboid(polarization=(0, 0, 0.1), dimension=(0.01, 0.01, 0.01))
 
-def magnetic_field_dipole(
-    pos: np.ndarray, dipole_moment: np.ndarray, magnet_center: np.ndarray
-):
-    r = pos - magnet_center
-    r_mag = np.linalg.norm(r, axis=-1, keepdims=True)
+B = magnet.getB(grid.grid) * u.T
 
-    # Avoid singularity
-    r_mag = np.maximum(r_mag, 0.01)
+grid.add_quantities(B_x=B[:,:,:,0], B_y=B[:,:,:,1], B_z=B[:,:,:,2])
 
-    r_hat = r / r_mag
-    m_dot_r = np.sum(dipole_moment * r_hat, axis=-1, keepdims=True)
+x0 = [[0.03, 0, 0]] * u.m
+v0 = [[0, 100, 100]] * u.m / u.s
+particle = Particle("p+")
 
-    B = (1 / r_mag**3) * (3 * m_dot_r * r_hat - dipole_moment)
-    return B
+termination_condition = TimeElapsedTerminationCondition(0.001 * u.second)
+save_routine = IntervalSaveRoutine(0.0000001 * u.second)
 
-def magnetic_field_dipole_single_pos(
-    pos: np.ndarray, dipole_moment: np.ndarray, magnet_center: np.ndarray
-):
-    r = pos - magnet_center
-    r_mag = np.linalg.norm(r, axis=-1, keepdims=True)
+simulation = ParticleTracker(
+    grid,
+    save_routine=save_routine,
+    termination_condition=termination_condition,
+    verbose=False,
+)
 
-    # Avoid singularity
-    r_mag = np.maximum(r_mag, 0.01)
+simulation.load_particles(x0, v0, particle)
+simulation.run()
 
-    r_hat = r / r_mag
-    m_dot_r = np.sum(dipole_moment * r_hat, axis=-1, keepdims=True)
+results = save_routine.results
 
-    B = (1 / r_mag**3) * (3 * m_dot_r * r_hat - dipole_moment)
-    return np.sum(B, axis=-2)
+pl = pv.Plotter()
+magpy.show(magnet, canvas=pl)
 
+particle_trajectory = results["x"][:, 0]
 
-def mag_field(pos: np.ndarray):
-    B = np.zeros(pos.shape, dtype=np.float32)
+print(particle_trajectory)
 
-    for x in np.linspace(-0.9, 0.9, 25):
-        for r in np.linspace(0, 2 * np.pi, 8, endpoint=False):
-            y = np.cos(r) * 0.5
-            z = np.sin(r) * 0.5
-
-            B += magnetic_field_dipole(pos, np.array([1.0, 0, 0]), np.array([x, y, z]))
-    
-    return B
-
-def mag_field_pos(pos: np.ndarray):
-    
-
-def screen_to_world(x, y):
-    return (x / WIDTH * 2 - 1), -(y / WIDTH * 2 - 1)
-
-
-def world_to_screen(x, y):
-    """Convert world coordinates to screen coordinates"""
-    screen_x = (x + 1.0) / 2.0 * WIDTH
-    screen_y = (-y + 1.0) / 2.0 * WIDTH  # Flip Y for screen coords
-    return int(screen_x), int(screen_y)
-
-
-def world_rect(x, y, w, h):
-    sx, sy = world_to_screen(x, y)
-    sw = int(w * WIDTH / 2)
-    sh = int(h * WIDTH / 2)
-    return sx, sy, sw, sh
-
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Magnets :)")
-    screen.fill(0)
-    background = None
-    B = None
-
-    if SHOW_FIELD:
-        background = pygame.Surface((WIDTH, HEIGHT))
-        background.fill(0)
-
-        screen_x = np.linspace(-1, 1, WIDTH, endpoint=False)
-        screen_y = np.linspace(-1, 1, HEIGHT, endpoint=False)
-        sx, sy = np.meshgrid(screen_x, screen_y)
-        pos = np.stack([sx, sy, np.zeros_like(sx)], axis=-1)
-
-        B = mag_field(pos)
-
-        for sx in range(0, WIDTH):
-            for sy in range(0, HEIGHT):
-                r, g, b = np.clip(np.abs(B[sy,sx]), 0, 255)
-
-                background.set_at((sx, sy), (int(r), int(g), int(b)))
-
-    p = np.array([-0.5, 0.0, 0.0])
-    q = 0.1
-    v = np.array([0.5, 0.0, 0.0])
-    dt = 0.0001
-
-    done = False
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        
-        if background:
-            screen.blit(background, (0, 0))
-        else:
-            screen.fill(0x000000)
-
-        pygame.draw.circle(screen, 0x00FF00, world_to_screen(p[0], p[1]), 3)
-
-        B_pos = mag_field(p)
-
-        F = q * np.cross(v, B_pos)
-
-        p += v * dt
-        v += F * dt
-
-        pygame.display.update()
-
-
-if __name__ == "__main__":
-    main()
+pl.add_lines(particle_trajectory * 1000, color='black', width=3, connected=True)
+#pl.add_lines(np.array([[0, 0, 0], [0, 0, 1]]), color='black')
+pl.show()
